4c6309c755f93127f5032462d7444c1d6510d571
Ghiftee <gigibetine@gmail.com>
2021-08-30 16:14:15 +0100

    Style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,24 +3,34 @@ import logo from '../logo.png';
 
 function header() {
   return (
-    <div>
-      <div>
-        <img src={logo} alt="logo" className="logo" />
-        <h1>Space Travelers&apos; Hub</h1>
+    <div className="d-flex align-items-center justify-content-between mx-2 py-3">
+      <div className="d-flex align-items-center">
+        <img
+          src={logo}
+          alt="logo"
+          className="logo me-2"
+          style={{
+            display: 'inline-block',
+          }}
+        />
+        <h2>Space Travelers&apos; Hub</h2>
       </div>
       <nav>
         <NavLink
           to="/rockets"
+          className="px-3"
         >
           Rockets
         </NavLink>
         <NavLink
           to="/missions"
+          className="px-3 border-end border-dark"
         >
           Missions
         </NavLink>
         <NavLink
           to="/profile"
+          className="px-3"
         >
           My Profile
         </NavLink>
