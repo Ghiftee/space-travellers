f56bf179fda4454a23674a311469e3a6313f9c65
Ghiftee <gigibetine@gmail.com>
2021-08-30 15:34:41 +0100

    Create routes and view components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import header from 'Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/rockets">
+          <Rockets />
+        </Route>
+        <Route path="/missions">
+          <Missions />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import './App.css';
+import { NavLink } from 'react-router-dom';
+import logo from '/logo.png';
+
+function header () {
+  return (
+    <div>
+      <div>
+        <img src={logo}/>
+        <h1>Space Travelers&apos; Hub</h1>
+      </div>
+      <nav>
+        <NavLink
+          to="/rockets"
+        >
+          Rockets
+        </NavLink>
+        <NavLink
+          to="/missions"
+        >
+          Missions
+        </NavLink>
+        <NavLink
+          to="/profile"
+        >
+          My Profile
+        </NavLink>
+      </nav>
+    </div>
+  );
+}
+
+export default header;
