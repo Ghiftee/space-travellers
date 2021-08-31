385d66a9e494117d67280ae59db7aea8c7d8a73a
Tolibjon <tolibjohn@gmail.com>
2021-08-31 18:24:41 +0500

    Created action creator function

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { reserveRocket } from '../redux/rockets/rockets';
 
-const Rocket = ({ rocket }) => (
-  <li key={rocket.id} className="d-flex m-2 p-1">
-    <img src={rocket.flickr_images[0]} alt={rocket.rocket_name} className="w-25 ms-2 me-3" />
-    <div className="flex-grow-3">
-      <h2>{rocket.rocket_name}</h2>
-      <p>{rocket.description}</p>
-      <button type="button" className="btn btn-primary">Reserve Rocket</button>
-    </div>
-  </li>
-);
+const Rocket = ({ rocket }) => {
+  const dispatch = useDispatch();
+
+  const handleReserve = (id) => {
+    dispatch(reserveRocket(id));
+  };
+
+  return (
+    <li key={rocket.id} className="d-flex m-2 p-1">
+      <img src={rocket.flickr_images[0]} alt={rocket.rocket_name} className="w-25 ms-2 me-3" />
+      <div className="flex-grow-3">
+        <h2>{rocket.rocket_name}</h2>
+        <p>{rocket.description}</p>
+        {rocket.reserved
+          ? <button type="button" className="btn btn-outline-secondary" onClick={() => handleReserve(rocket.id)}>Cancel Reservation</button>
+          : <button type="button" className="btn btn-primary" onClick={() => handleReserve(rocket.id)}>Reserve Rocket</button>}
+      </div>
+    </li>
+  );
+};
 
 export default Rocket;
 
 Rocket.propTypes = {
   rocket: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    flickr_images: PropTypes.arrayOf().isRequired,
+    id: PropTypes.number.isRequired,
+    flickr_images: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
     rocket_name: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
+    reserved: PropTypes.bool,
   }).isRequired,
 };
