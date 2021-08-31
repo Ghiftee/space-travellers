31cabc6d05e40b45f630804a2c63557b32a036fc
Tolibjon <tolibjohn@gmail.com>
2021-08-31 14:42:59 +0500

    Dispatch an action to store fetched data in Redux store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import Header from './components/Header';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
@@ -7,20 +9,22 @@ import Profile from './components/Profile';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/rockets">
-          <Rockets />
-        </Route>
-        <Route path="/missions">
-          <Missions />
-        </Route>
-        <Route path="/profile">
-          <Profile />
-        </Route>
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Rockets />
+          </Route>
+          <Route path="/missions">
+            <Missions />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../redux/rockets/rockets';
 
-const Rockets = () => (
-  <div>
-    Rockets
-  </div>
-);
+const Rockets = () => {
+  const rockets = useSelector((state) => state.rocketsReducer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!rockets.length > 0) {
+      dispatch(fetchRockets());
+    }
+  }, []);
+
+  return (
+    <div>
+      Rockets
+    </div>
+  );
+};
 
 export default Rockets;
