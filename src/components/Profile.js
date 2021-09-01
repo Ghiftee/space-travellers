c1c1bbe1200070cebf3d0bd27065fc7b75f8cd21
Tolibjon <tolibjohn@gmail.com>
2021-09-01 09:22:27 +0500

    Add functionality to display reserved rockets in Profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import {
+  Container,
+  Row,
+  Col,
+  ListGroup,
+} from 'react-bootstrap';
 
-const Profile = () => (
-  <div>
-    Profile
-  </div>
-);
+const Profile = () => {
+  const rocketState = useSelector((state) => state.rocketsReducer);
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h2>Missions</h2>
+        </Col>
+        <Col>
+          <h2>My Rockets</h2>
+          <ListGroup>
+            {rocketState.filter((rocket) => rocket.reserved).map((rocket) => <ListGroup.Item key={rocket.id} className="list-group-item">{rocket.rocket_name}</ListGroup.Item>)}
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Profile;
